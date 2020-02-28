5d0a04237867963fed6f72351af16b98b51b1da1
Luke Morris <lukemorris@students.berkeley.net>
2020-02-28 14:26:14 -0800

    update

--- src/Runner.java
+++ src/Runner.java
@@ -2,36 +2,36 @@ public class Runner {
     public static void main(String[] args){
         School bh = new School("Berkeley High");
 
-        Teacher albrecht = new Teacher("Albrecht", 1, "Math");
-        Teacher albin = new Teacher("Albinson", 2, "Computer Science");
-        Teacher kaku = new Teacher("Kaku", 3, "English");
+        Teacher albrecht = new Teacher("Albinsum", 1, "Math");
+        Teacher albin = new Teacher("Albingram", 2, "Computer Science");
+        Teacher kaku = new Teacher("Albinglish", 3, "English");
 
         Section math = new Section("Math");
         Section com = new Section("Computer Science");
         Section eng = new Section("English");
 
-        Student abby = new Student("Abby", 4, 11);
-        Student eva = new Student("Eva", 5, 11);
-        Student nina = new Student("Nina", 6, 11);
-        Student lou = new Student("Louisa", 7, 11);
-        Student pema = new Student("Pema", 8, 11);
-        Student ella = new Student("Ella", 9, 11);
+        Student albin2 = new Student("Mr.Albinson 2.0", 4, 11);
+        Student albin3 = new Student("Mr.Albinson 3.0", 5, 11);
+        Student albin4 = new Student("Mr.Albinson 4.0", 6, 11);
+        Student albin5 = new Student("Mr.Albinson 5.0", 7, 11);
+        Student albin6 = new Student("Mr.Albinson 6.0", 8, 11);
+        Student albin7 = new Student("Mr.Albinson 7.0", 9, 11);
 
 
         math.setTeacher(albrecht);
         com.setTeacher(albin);
         eng.setTeacher(kaku);
 
-        math.addStudents(abby);
-        math.addStudents(ella);
+        math.addStudents(albin2);
+        math.addStudents(albin3);
 
-        com.addStudents(eva);
-        com.addStudents(nina);
-        com.addStudents(abby);
+        com.addStudents(albin4);
+        com.addStudents(albin5);
+        com.addStudents(albin6);
 
-        eng.addStudents(eva);
-        eng.addStudents(lou);
-        eng.addStudents(pema);
+        eng.addStudents(albin6);
+        eng.addStudents(albin7);
+        eng.addStudents(albin4);
 
         System.out.println(math.toString());
         System.out.println(com.toString());
